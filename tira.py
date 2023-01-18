9fa9a01ffff4d3efdf12b5daf75199b51c94b7ca
TimelessP <>
2023-01-18 21:10:24 +0000

    Initial commit.

--- tira.py
+++ tira.py
@@ -1,5 +1,6 @@
 import os.path
 import pickle
+import re
 from typing import Dict
 
 from model import Issue
@@ -14,6 +15,7 @@ class Controller:
         self.issues: Dict[str, Issue] = {}
         self.is_running = True
         self.next_id = 1
+        self.tira_space = "TI"
         self.load()
 
     def save(self):
@@ -36,16 +38,28 @@ class Controller:
                 "edit": self.edit,
                 "exit": self.quit,
                 "help": self.help,
+                "space": self.space,
+                "spaces": self.spaces,
             }
             print("")
-            action, *args = input("> ").split()
+            action, *args = input(f"tira: {self.tira_space}> ").split()
             if action not in actions:
                 print("Unknown action")
                 continue
             actions[action](*args)
 
+    def space(self, space=None):
+        if not space or not re.search(r"^[A-Z]{2,3}$", space):
+            print("Space must be two or three capital letters")
+            return
+        self.tira_space = space
+
+    def spaces(self):
+        spaces = set(key.split("-")[0] for key in self.issues)
+        print(" ".join(sorted(spaces)))
+
     def add(self):
-        key = f"TI-{self.next_id}"
+        key = f"{self.tira_space}-{self.next_id}"
         self.next_id += 1
         description = input("Description: ")
         self.issues[key] = Issue(key, description)
@@ -53,12 +67,12 @@ class Controller:
         print("Added issue")
 
     def list(self):
-        for issue in self.issues.values():
+        for issue in [iss for iss in self.issues.values() if iss.key.startswith(self.tira_space)]:
             formatted_date = issue.created.strftime("%Y-%m-%d %H:%M")
             print(f"{issue.key}\t{formatted_date}\t{repr(issue.description)}")
 
     def show(self, key):
-        issue = self.issues.get(key, None)
+        issue = self.issues.get(key, None) or self.issues.get(f"{self.tira_space}-{key}", None)
         if issue is None:
             print(ISSUE_NOT_FOUND)
             return
@@ -67,7 +81,7 @@ class Controller:
         print(issue.created)
 
     def delete(self, key):
-        issue = self.issues.get(key, None)
+        issue = self.issues.get(key, None) or self.issues.get(f"{self.tira_space}-{key}", None)
         if issue is None:
             print(ISSUE_NOT_FOUND)
             return
@@ -76,7 +90,7 @@ class Controller:
         print("Deleted issue")
 
     def edit(self, key):
-        issue = self.issues.get(key, None)
+        issue = self.issues.get(key, None) or self.issues.get(f"{self.tira_space}-{key}", None)
         if issue is None:
             print(ISSUE_NOT_FOUND)
             return
@@ -91,13 +105,16 @@ class Controller:
         self.is_running = False
 
     def help(self):
-        print("add")
-        print("list")
-        print("show <key>")
-        print("delete <key>")
-        print("edit <key>")
-        print("exit")
-        print("help")
+        print("Valid actions:")
+        print("\tadd            Add a new issue.")
+        print("\tlist           List all issues.")
+        print("\tshow <key>     Show an issue.")
+        print("\tdelete <key>   Delete an issue.")
+        print("\tedit <key>     Edit an issue.")
+        print("\texit           Exit the program.")
+        print("\thelp           Show this help.")
+        print("\tspace <space>  Set the space for new issues.")
+        print("\tspaces         List all spaces.")
 
 
 if __name__ == "__main__":
