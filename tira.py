c1ae8f3137c8cbbfa53610d9a1d6a2383d5fa371
TimelessP <>
2023-01-28 13:00:20 +0000

    Various bug fixes.

--- tira.py
+++ tira.py
@@ -29,28 +29,33 @@ class Controller:
                 self.next_id = max(int(key.split("-")[1]) for key in self.issues) + 1
 
     def run(self):
+        actions = {
+            "add": self.add,
+            "list": self.list,
+            "show": self.show,
+            "delete": self.delete,
+            "edit": self.edit,
+            "exit": self.quit,
+            "help": self.help,
+            "space": self.space,
+            "spaces": self.spaces,
+            "find": self.find,
+        }
         while self.is_running:
-            actions = {
-                "add": self.add,
-                "list": self.list,
-                "show": self.show,
-                "delete": self.delete,
-                "edit": self.edit,
-                "exit": self.quit,
-                "help": self.help,
-                "space": self.space,
-                "spaces": self.spaces,
-                "find": self.find,
-            }
             print("")
             action, *args = input(f"tira: {self.tira_space}> ").split() or " "
-            if action not in actions:
-                if action.strip():
-                    print("Invalid action")
-                continue
-            actions[action](*args)
 
-    def find(self, text=None):
+            for action_name, handler in actions.items():
+                if action_name.startswith(action):
+                    break
+            else:
+                print("Invalid action")
+                continue
+
+            handler(*args)
+
+    def find(self, *args):
+        text = " ".join(args)
         if not text:
             print("Text to find is required")
             return
@@ -62,83 +67,105 @@ class Controller:
                 if is_more.lower() == "n":
                     break
 
-    def space(self, space=None):
+    def space(self, *args):
+        space = " ".join(args)
         if not space or not re.search(r"^[A-Z]{2,3}$", space):
             print("Space must be two or three capital letters")
             return
         self.tira_space = space
 
-    def spaces(self):
-        spaces = set(key.split("-")[0] for key in self.issues)
+    def spaces(self, *args):
+        filter_text = " ".join(args)
+        if filter_text:
+            spaces = set(key.split("-")[0] for key in self.issues if filter_text.lower() in key.lower())
+        else:
+            spaces = set(key.split("-")[0] for key in self.issues)
+
+        if not spaces:
+            print("No spaces found")
+            return
+
         print(" ".join(sorted(spaces)))
 
-    def add(self):
+    def add(self, *args):
         key = f"{self.tira_space}-{self.next_id}"
         self.next_id += 1
-        description = input("Description: ")
+        description = " ".join(args) if args else input("Description: ")
         self.issues[key] = Issue(key, description)
         self.save()
-        print("Added issue")
+        print(f"Added issue {key}")
 
-    def list(self):
+    def list(self, *args):
+        filter_text = " ".join(args)
         for issue in [iss for iss in self.issues.values() if iss.key.startswith(self.tira_space)]:
             formatted_date = issue.created.strftime("%Y-%m-%d %H:%M")
-            print(f"{issue.key}\t{formatted_date}\t{repr(issue.description)}")
+            line = f"{issue.key}\t{formatted_date}\t{repr(issue.description)}"
+            if not filter or filter_text.lower() in line.lower():
+                print(line)
 
-    def show(self, key=None):
+    def show(self, *args):
+        key = " ".join(args)
         if not key:
             print(KEY_IS_REQUIRED)
             return
+
         issue = self.issues.get(key, None) or self.issues.get(f"{self.tira_space}-{key}", None)
         if issue is None:
             print(ISSUE_NOT_FOUND)
             return
+
         print(issue.key)
         print(issue.description)
         print(issue.created)
 
-    def delete(self, key=None):
+    def delete(self, *args):
+        key = " ".join(args)
         if not key:
             print(KEY_IS_REQUIRED)
             return
+
         issue = self.issues.get(key, None) or self.issues.get(f"{self.tira_space}-{key}", None)
         if issue is None:
             print(ISSUE_NOT_FOUND)
             return
+
+        print(f"Deleted issue {issue.key}")
         del self.issues[issue.key]
         self.save()
-        print("Deleted issue")
 
-    def edit(self, key=None):
+    def edit(self, *args):
+        key = " ".join(args)
         if not key:
             print(KEY_IS_REQUIRED)
             return
+
         issue = self.issues.get(key, None) or self.issues.get(f"{self.tira_space}-{key}", None)
         if issue is None:
             print(ISSUE_NOT_FOUND)
             return
+
         print("Editing issue", issue.key)
         print(f"Description: {issue.description}")
         issue.description = input("New description (Enter=no change): ") or issue.description
         self.save()
         print("Edited issue")
 
-    def quit(self):
+    def quit(self, *_):
         self.save()
         self.is_running = False
 
-    def help(self):
+    def help(self, *_):
         print("Valid actions:")
-        print("\tadd            Add a new issue.")
-        print("\tlist           List all issues.")
-        print("\tshow <key>     Show an issue.")
-        print("\tdelete <key>   Delete an issue.")
-        print("\tedit <key>     Edit an issue.")
-        print("\texit           Exit the program.")
-        print("\thelp           Show this help.")
-        print("\tspace <space>  Set the space for new issues.")
-        print("\tspaces         List all spaces.")
-        print("\tfind <text>    Find issues containing text.")
+        print("\tadd [<text>]     Add a new issue.")
+        print("\tlist [<text>]    List issues in the current space.")
+        print("\tshow <key>       Show an issue.")
+        print("\tdelete <key>     Delete an issue.")
+        print("\tedit <key>       Edit an issue.")
+        print("\texit             Exit the program.")
+        print("\thelp             Show this help.")
+        print("\tspace <space>    Set the space for new issues.")
+        print("\tspaces [<text>]  List spaces.")
+        print("\tfind <text>      Find issues in all spaces that contain the text.")
 
         print("Current space:", self.tira_space)
 
